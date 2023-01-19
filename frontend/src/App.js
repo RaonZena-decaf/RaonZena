b2a9ab44f82a43b9eaa73f1d07a4898f6beb6c55
limgilhyun <limgilhyun@naver.com>
2023-01-19 15:27:14 +0900

    Feat: 404 페이지 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Counter from './routes/Counter'
+import Counter from "./routes/Counter";
+import PageNotFound from "./routes/PageNotFound";
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <Route path="/room/:roomId/picture" element={} />
         <Route path="/live" element={} />
         <Route path="/profile/:userNo" element={} /> */}
-        <Route path="/" element={< Counter />} />
-        {/* <Route path="*" element={< NotFound />} /> */}
+        <Route path="/" element={<Counter />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
