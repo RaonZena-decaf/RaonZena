da09819e9868e18144e85155fe0180b8957e8c77
choijiyeon <choiwldus@naver.com>
2023-01-24 00:07:16 +0900

    Feat: 게임접속 구현

--- backend/raonzena/src/main/java/com/ssafy/raonzena/db/repository/LiveRepository.java
+++ backend/raonzena/src/main/java/com/ssafy/raonzena/db/repository/LiveRepository.java
@@ -10,8 +10,9 @@ import java.util.Map;
 /**
  * 실행중인 게임방 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
  */
-
 public interface LiveRepository {
     // 현재 실행중인 방 조회
     List<LiveRoomInfoRes> selectRooms(Map<String, Object> conditions);
+    // 유저 게임 참가 가능 여부 조회
+    boolean  isAccessible(int roomNo, int sessionHeadCount); /////////세션정보 필요//////////
 }
