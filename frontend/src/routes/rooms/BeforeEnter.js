29365009f41de63ee7a2ed33ec63535a778bf06b
Young312886 <hongym0497@gmail.com>
2023-01-25 14:58:04 +0900

    Design : 방 입력 창 배경 색 추가

--- frontend/src/routes/rooms/BeforeEnter.js
+++ frontend/src/routes/rooms/BeforeEnter.js
@@ -44,47 +44,49 @@ function BeforeEnter() {
   };
 
   return (
-    <div className={style.container}>
-      <div className={style.innercontainer}>
-        <div className={style.leftcontainer}>
-          <h2 className={style.header}>
-            <span>{roomname}</span>에 참가 준비 중 입니다
-          </h2>
-          <div className={style.bottom}>
-            <div className={style.textcont}>
-              <FaUser className={style.highlight} /> 현재 {attend}/6 명이 방에
-              있습니다
+    <div className={style.background}>
+      <div className={style.container}>
+        <div className={style.innercontainer}>
+          <div className={style.leftcontainer}>
+            <h2 className={style.header}>
+              <span>{roomname}</span>에 참가 준비 중 입니다
+            </h2>
+            <div className={style.bottom}>
+              <div className={style.textcont}>
+                <FaUser className={style.highlight} /> 현재 {attend}/6 명이 방에
+                있습니다
+              </div>
+              <div className={style.textcont}>
+                카메라와 마이크 권한을 요청합니다. <br />
+                요청 메세지를 확인하여 주세요.
+              </div>
+              <div className={style.textcont}>
+                만약 오류가 발생하였을 경우, <br /> 홈 화면으로 이동 후 재접속
+                하시기 바랍니다
+              </div>
             </div>
-            <div className={style.textcont}>
-              카메라와 마이크 권한을 요청합니다. <br />
-              요청 메세지를 확인하여 주세요.
-            </div>
-            <div className={style.textcont}>
-              만약 오류가 발생하였을 경우, <br /> 홈 화면으로 이동 후 재접속
-              하시기 바랍니다
-            </div>
+            <button onClick={backOnClick}>나가기</button>
           </div>
-          <button onClick={backOnClick}>나가기</button>
-        </div>
 
-        <div className={style.rightcontainer}>
-          <div className={style.video}>화상</div>
-          <div className={style.accessory}>
-            {mic ? (
-              <FaMicrophoneAlt onClick={micOnClick} />
-            ) : (
-              <FaMicrophoneAltSlash onClick={micOnClick} />
-            )}
+          <div className={style.rightcontainer}>
+            <div className={style.video}>화상</div>
+            <div className={style.accessory}>
+              {mic ? (
+                <FaMicrophoneAlt onClick={micOnClick} />
+              ) : (
+                <FaMicrophoneAltSlash onClick={micOnClick} />
+              )}
 
-            {camera ? (
-              <FaVideo onClick={cameraOnClick} />
-            ) : (
-              <FaVideoSlash onClick={cameraOnClick} />
-            )}
+              {camera ? (
+                <FaVideo onClick={cameraOnClick} />
+              ) : (
+                <FaVideoSlash onClick={cameraOnClick} />
+              )}
+            </div>
+            <button className={style.enter} onClick={enterOnClick}>
+              Enter
+            </button>
           </div>
-          <button className={style.enter} onClick={enterOnClick}>
-            Enter
-          </button>
         </div>
       </div>
     </div>
