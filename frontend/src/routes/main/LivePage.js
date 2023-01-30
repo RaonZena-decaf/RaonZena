7dd82704aa76f508d5fc1808162cf456d8af9cf0
alsth4 <alsth4@naver.com>
2023-01-30 10:40:02 +0900

    Fix: [라이브 페이지] 게림방 리스트 수정 및 완성

--- frontend/src/routes/main/LivePage.js
+++ frontend/src/routes/main/LivePage.js
@@ -14,14 +14,6 @@ const LivePage = () => {
             <GameRooms className={styles.center}></GameRooms>
           </div>
         </div>
-
-        <div className={styles.background3}>
-          <div className={styles.NowPlayingTitle}>
-            {`지금 `}
-            <b id={styles.pinkcolor}> 노는 중</b>
-            <GameRooms className={styles.center}></GameRooms>
-          </div>
-        </div>
       </div>
     </div>
   );
