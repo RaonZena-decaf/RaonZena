ad52d544816a50a2bc72866ad3a5d377c8293fb2
alsth4 <alsth4@naver.com>
2023-01-30 16:54:43 +0900

    Feature: Followings 참여한 게임방 구현

---
+++ frontend/src/components/Followings/Item.js
@@ -0,0 +1,29 @@
+import { style } from "@mui/system";
+import React, { useState, useNavigate } from "react";
+import styles from "./Followings.module.css";
+
+export default function Item(props) {
+  return (
+    <div className={styles.FollowingsInfo}>
+      <div>
+        <img
+          className={styles.ProfileSize}
+          src={props.userImage}
+          alt="Profile"
+        />
+      </div>
+      <div className={styles.FollowingInfoFontSize}>
+        <p>LV {props.level}</p>
+      </div>
+      <div className={styles.FollowingInfoFontSize}>
+        <p>{props.userName}</p>
+      </div>
+      <div>
+        {props.isOnline && (
+          <img className={styles.OnlineSize} src="Online.svg" alt="Online" />
+        )}
+      </div>
+      <div className={styles.Fo}></div>
+    </div>
+  );
+}
--- frontend/src/routes/main/LivePage.js
+++ frontend/src/routes/main/LivePage.js
@@ -1,6 +1,7 @@
 import styles from "./LivePage.module.css";
 import Navbar from "../../components/navbar/navbar";
 import GameRooms from "../../components/GameRoom/GameRoom";
+import FollowingList from "../../components/Followings/FollowingList";
 
 const LivePage = () => {
   return (
@@ -12,6 +13,21 @@ const LivePage = () => {
             {`지금 `}
             <b id={styles.pinkcolor}> 노는 중</b>
             <GameRooms className={styles.center}></GameRooms>
+          </div>
+        </div>
+        <div>
+          <div className={styles.displayFlex}>
+            <div className={styles.followingsInGameList}>
+              {`팔로잉이 `}
+              <b id={styles.pinkcolor}> 주최 중</b>
+            </div>
+            <div className={styles.followingsList}>
+              <div className={styles.No}>
+                {`당신의 `}
+                <b id={styles.pinkcolor}> 팔로잉</b>
+                <FollowingList></FollowingList>
+              </div>
+            </div>
           </div>
         </div>
       </div>
