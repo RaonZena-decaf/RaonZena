b118840ab33eb5ef029f52898a2760521939d7aa
unknown <driculer@gmail.com>
2023-01-30 09:20:32 +0900

    Feat: 로딩 이미지 FA로 대체

--- frontend/src/components/room/ChatingSubjectLoading.js
+++ frontend/src/components/room/ChatingSubjectLoading.js
@@ -1,13 +1,11 @@
 import style from "./ChatingSubjectLoading.module.css";
+import { FaCircleNotch } from "react-icons/fa";
+
 
 function ChatingSubjectLoading() {
   return (
     <div className={style.background}>
-      <img
-        alt="로딩중"
-        src="../img/Loading.png"
-        className={style.logoImg}
-      />
+      <FaCircleNotch className={style.logoImg}/>
       <div className={style.fontsize}>주제를 고르고 있어요...</div>
     </div>
   );
