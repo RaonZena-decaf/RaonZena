3fd341c6013afa4ebddd15f80353e15b376f4e8d
limgilhyun <limgilhyun@naver.com>
2023-01-31 16:02:18 +0900

    Feat:프로필 모달

---
+++ frontend/src/components/ProfilePage/ProfilePostDetail.js
@@ -0,0 +1,68 @@
+import styles from "./ProfilePostDetail.module.css";
+import { FaCrown } from "react-icons/fa";
+import { useState } from "react";
+
+function ProfilePostDetail({ nowContent }) {
+  //임시값, axios 구현 후 비워놓을 것
+  const [users, serUsers] = useState([
+    { user_name: "김찬빈", user_image_url: "../profile/profileimg.png" },
+    { user_name: "김민소", user_image_url: "../profile/profileimg.png" },
+    { user_name: "최지연", user_image_url: "../profile/profileimg.png " },
+  ]);
+
+  // nowContent.firstUser
+  // nowContent.SecondUser
+  // nowContent.ThirdUser
+  // 각각의 유저 이름과 유저 프로필 사진 데이터를 요청. axios로 요청
+
+  return (
+    <div className={styles.background}>
+      <p className={styles.text}>{nowContent.title}</p>
+      <img src={nowContent.board_image_url} className={styles.img} alt="img" />
+      <p className={styles.text2}>{nowContent.create_dtm}의 기록</p>
+      {nowContent.first_user && (
+        <div className={styles.OutContainer}>
+          <FaCrown className={styles.icon} />
+          <div className={styles.LargeContainer}>
+            <div className={styles.SmallContainer}>
+              <img
+                src={users[1].user_image_url}
+                alt="img"
+                className={styles.profileImg}
+              />
+              <span>{users[1].user_name}</span>
+              <div className={styles.InnerContainer2}>
+                <span>2</span>
+              </div>
+            </div>
+            <div className={styles.SmallContainer}>
+              <img
+                src={users[0].user_image_url}
+                alt="img"
+                className={styles.profileImg}
+              />
+              <span>{users[0].user_name}</span>
+              <div className={styles.InnerContainer1}>
+                <span>1</span>
+              </div>
+            </div>
+            <div className={styles.SmallContainer}>
+              <img
+                src={users[2].user_image_url}
+                alt="img"
+                className={styles.profileImg}
+              />
+              <span>{users[2].user_name}</span>
+              <div className={styles.InnerContainer3}>
+                <span>3</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={styles.textarea}>{nowContent.content}</div>
+    </div>
+  );
+}
+
+export default ProfilePostDetail;
