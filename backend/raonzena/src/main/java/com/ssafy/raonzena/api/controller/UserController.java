072dc238bf4326a872ba997635e1c24f2549c22c
choijiyeon <choiwldus@naver.com>
2023-02-03 12:24:56 +0900

    Fix: 로그인 http 메소드 변경

--- backend/raonzena/src/main/java/com/ssafy/raonzena/api/controller/UserController.java
+++ backend/raonzena/src/main/java/com/ssafy/raonzena/api/controller/UserController.java
@@ -5,9 +5,7 @@ import com.ssafy.raonzena.api.response.UserRes;
 import com.ssafy.raonzena.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -17,7 +15,7 @@ public class UserController {
     UserService userService;
 
 
-    @GetMapping("/kakao/callback")
+    @PostMapping("/kakao/callback")
     public ResponseEntity<UserRes> kakaoLogin(String code){
         //authorizeCode : 카카오 서버로부터 받은 인가 코드
         return ResponseEntity.ok(userService.KaKaoLogin(code));
