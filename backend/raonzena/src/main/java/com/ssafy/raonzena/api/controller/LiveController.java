5109dd5ae39615ec186e52a13b4af575799d72e7
choijiyeon <choiwldus@naver.com>
2023-02-03 16:28:59 +0900

    Fix: openvidu 서버 url 변경

--- backend/raonzena/src/main/java/com/ssafy/raonzena/api/controller/LiveController.java
+++ backend/raonzena/src/main/java/com/ssafy/raonzena/api/controller/LiveController.java
@@ -32,7 +32,7 @@ public class LiveController {
     private UserService userService;
 
     //openvidu_url
-    @Value("https://i8a507.p.ssafy.io")  //https://i8a507.p.ssafy.io:3478
+    @Value("https://i8a507.p.ssafy.io:8443")  //https://i8a507.p.ssafy.io:3478
     private String OPENVIDU_URL;
 
     //시크릿 키
