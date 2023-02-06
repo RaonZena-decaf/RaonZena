1096974001cfd63d0103675ddf789ab7a70d8fe5
Young312886 <hongym0497@gmail.com>
2023-02-06 09:55:53 +0900

    Feat: 게임 방 이동시 시그널 송신

--- frontend/src/routes/rooms/MainRoom.js
+++ frontend/src/routes/rooms/MainRoom.js
@@ -1,5 +1,5 @@
 import styles from "./MainRoom.module.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MenuBar from "../../components/room/MenuBar";
 import ChattingBar from "../../components/room/ChattingBar";
 import { OpenVidu } from "openvidu-browser";
@@ -9,6 +9,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import axios from "axios";
 import Loading from "../../components/room/MainLoading";
+import Catchmind from "../../components/game/catchmind";
 const OPENVIDU_SERVER_URL = "https://i8a507.p.ssafy.io:8443";
 const OPENVIDU_SERVER_SECRET = "RAONZENA";
 
@@ -17,7 +18,7 @@ function MainRoom(props) {
   const [session, setSession] = useState(undefined);
   const [OV, setOV] = useState(undefined);
   const [subscribes, setSubscribes] = useState([]);
-  const [userName, setUserName] = useState(`User ${getRandomInt(1,100)}`);
+  const [userName, setUserName] = useState(`User ${getRandomInt(1, 100)}`);
   const [roomId, setroomId] = useState("Roomc");
   const [publisher, setPublisher] = useState(undefined);
   const [openvidu, setOpenvidu] = useState(undefined);
@@ -31,16 +32,18 @@ function MainRoom(props) {
 
   //메인메뉴 모달을 위한 함수
   const deleteSubscriber = (streamManager) => {
-    setSubscribes(prev => prev.filter(stream => stream.streamManager !== streamManager))
-    }
-// 임시 사용자 이름 랜덤으로 부여
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-}
-  const toggleDevice =  async () => {
-    console.log('Toggle device')
+    setSubscribes((prev) =>
+      prev.filter((stream) => stream.streamManager !== streamManager)
+    );
+  };
+  // 임시 사용자 이름 랜덤으로 부여
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  }
+  const toggleDevice = async () => {
+    console.log("Toggle device");
     // try {
     //   let devices = await OV.getDevices()
     //   let videoDevices = devices.filter(device => device.kind === 'videoinput')
@@ -64,16 +67,16 @@ function getRandomInt(min, max) {
     // } catch (error) {
     //   console.log(error)
     // }
-  }
+  };
   useEffect(() => {
     const OV = new OpenVidu();
-    setOV(OV)
+    setOV(OV);
     const after = new Promise((resolve, reject) => {
       const mySession = OV.initSession();
 
       setTimeout(() => {
         resolve(mySession);
-      }, 1000); 
+      }, 1000);
     });
     after
       .then((mySession) => {
@@ -85,10 +88,11 @@ function getRandomInt(min, max) {
         mySession.on("streamCreated", (event) => {
           const subscriber = mySession.subscribe(event.stream, undefined);
           setSubscribes((oldArray) => [...oldArray, subscriber]);
+          console.log("new enterance", subscribes);
         });
 
         mySession.on("streamDestroyed", (event) => {
-          console.log('manager', event.stream.streamManager)
+          console.log("manager", event.stream.streamManager);
           deleteSubscriber(event.stream.streamManager);
         });
 
@@ -96,8 +100,10 @@ function getRandomInt(min, max) {
           console.warn(exception);
           // 어떤 이유인지 왜 비디오가 없는 유저에 대해서 예외처리가 되질 않습니다. 추가적인 확인 이후에 해야함
         });
-        mySession.on("signal:gameChange", (event) => {})
-
+        mySession.on("signal:gameChange", (event) => {
+          console.log(event.data)
+          setGameName(event.data.gamename)
+        });
 
         // 토큰 발행 및 소켓 접속
         const getToken = () => {
@@ -156,7 +162,7 @@ function getRandomInt(min, max) {
                 }
               )
               .then((response) => {
-                console.log('createdtoken', response.data.token)
+                console.log("createdtoken", response.data.token);
                 resolve(response.data.token);
               })
               .catch((error) => reject(error));
@@ -166,8 +172,7 @@ function getRandomInt(min, max) {
         getToken().then((token) => {
           mySession
             .connect(token, { clientData: userName })
-            .then(
-              async () => {
+            .then(async () => {
               // --- 5) Get your own camera stream ---
               const publisher = await OV.initPublisherAsync(undefined, {
                 audioSource: undefined, // The source of audio. If undefined default microphone
@@ -178,11 +183,11 @@ function getRandomInt(min, max) {
                 frameRate: 30, // The frame rate of your video
                 insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
                 mirror: false, // Whether to mirror your local video or not
+                streamId: userName, // 다른 것들과 구분하기 위한 변수값
               });
 
               // --- 6) Publish your stream ---
               mySession.publish(publisher);
-
               // Obtain the current video device in use
               // const devices = await OV.getDevices();
               // const videoDevices = devices.filter(
@@ -243,21 +248,23 @@ function getRandomInt(min, max) {
     navigate("/live");
   };
   useEffect(() => {
-    console.log(publisher)
-    setVideoList({...subscribes, publisher})
+    console.log(publisher);
+    setVideoList({ ...subscribes, publisher });
     setOpenvidu({ session, videoList, userName });
   }, [session, publisher, userName, subscribes]);
   // 신호에 따른 화면 렌더링 변화
   const ChangeGame = (gamename) => {
     const data = {
       gamename: gamename,
-    }
+    };
     setGameName(gamename);
     openvidu.session.signal({
       data: JSON.stringify(data),
-      type: "chat",
+      type: "gameChange",
     });
-  }
+  };
+
+
   return (
     <div className={styles.background}>
       {session !== undefined ? (
@@ -273,7 +280,14 @@ function getRandomInt(min, max) {
             ))}
           </div>
 
-          <MenuBar toggleBar={toggleBar} exitaction={leaveSession} camera={props.camera} mic={props.mic} toggleDevice={toggleDevice} ChangeGame={ChangeGame} />
+          <MenuBar
+            toggleBar={toggleBar}
+            exitaction={leaveSession}
+            camera={props.camera}
+            mic={props.mic}
+            toggleDevice={toggleDevice}
+            ChangeGame={ChangeGame}
+          />
           <ChattingBar
             openChatting={openChatting}
             toggleBar={toggleBar}
