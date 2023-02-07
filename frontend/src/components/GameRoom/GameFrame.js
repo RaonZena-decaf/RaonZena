c4258aaa58c3ab9ce22f65a1975ee530aec429c8
limgilhyun <limgilhyun@naver.com>
2023-02-07 13:19:08 +0900

    Feat:정답확인

--- frontend/src/components/GameRoom/GameFrame.js
+++ frontend/src/components/GameRoom/GameFrame.js
@@ -3,17 +3,25 @@ import React, { useState, useEffect } from "react";
 import GameFrameLeft from "./GameFrameLeft";
 import GameFrameRight from "./GameFrameRight";
 
-function GameFrame({gamename}) {
+function GameFrame({ gamename }) {
   const [start, setStart] = useState(false);
   const startHandler = () => {
     setStart(true);
   };
   const [result, setResult] = useState("");
+  // const resetResult = () => {
+  //   setResult("");
+  // };
   return (
     <div className={styles.container}>
       <h1 className={styles.font}>인물퀴즈</h1>
       <div className={styles.container2}>
-        <GameFrameLeft start={start} result={result} setResult={setResult} gamename={gamename} />
+        <GameFrameLeft
+          start={start}
+          result={result}
+          setResult={setResult}
+          gamename={gamename}
+        />
         <GameFrameRight
           start={start}
           startHandler={startHandler}
