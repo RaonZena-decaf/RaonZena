082aa761cd3c1572a3ac2f84e31d2037bb9c30da
Young312886 <hongym0497@gmail.com>
2023-02-08 12:18:12 +0900

    Fix : 정보 출력 위한 값: :

--- frontend/src/routes/rooms/BeforeEnter.js
+++ frontend/src/routes/rooms/BeforeEnter.js
@@ -15,6 +15,8 @@ import {
 
 function BeforeEnter(props) {
   const { state } = useLocation();
+  console.log('state',state)
+  console.log('props',props)
   const user = useSelector((store) => store.userData);
   const [userName, setUserName] = useState(user.userId);
   const [roomId, setroomId] = useState(state);
@@ -22,7 +24,7 @@ function BeforeEnter(props) {
   const [camera, setCamera] = useState(true);
   const navigate = useNavigate();
   // 방 정보 세팅
-  
+
   // 이전 페이지로 돌아가기
   const backOnClick = () => {
     navigate(-1);
