c6ec53f2dfcba6f13addf193dd53e8d80d151280
Young312886 <hongym0497@gmail.com>
2023-02-14 14:59:31 +0900

    Fix : 미러 모드 해제

--- frontend/src/routes/rooms/MainRoom.js
+++ frontend/src/routes/rooms/MainRoom.js
@@ -163,7 +163,7 @@ function MainRoom(props) {
                 resolution: "640x480", // The resolution of your video
                 frameRate: 30, // The frame rate of your video
                 insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
-                mirror: true, // Whether to mirror your local video or not
+                mirror: false, // Whether to mirror your local video or not
                 streamId: userName, // 다른 것들과 구분하기 위한 변수값
                 host : state.host
               });
