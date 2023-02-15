1a490e418fb977629b55eb40787eb6e5326bc10b
Young312886 <hongym0497@gmail.com>
2023-02-15 22:12:32 +0900

    Fix : 라이브 페이지로 redirect

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -5,7 +5,7 @@ function PrivateRoute({ authenticated, component: Component }) {
   return authenticated ? (
     Component
   ) : (
-    <Navigate to="/" {...alert("로그인이 필요한 페이지 입니다")} />
+    <Navigate to="/live" {...alert("로그인이 필요한 페이지 입니다")} />
   );
 }
 
