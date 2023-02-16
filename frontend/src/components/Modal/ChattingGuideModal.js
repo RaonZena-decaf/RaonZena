3cd453b9b91fe4955ce3f11e8c5f909b28642ea3
unknown <driculer@gmail.com>
2023-02-16 14:34:55 +0900

    Fix:작성글자수 제한

--- frontend/src/components/Modal/ChattingGuideModal.js
+++ frontend/src/components/Modal/ChattingGuideModal.js
@@ -17,14 +17,22 @@ function ChattingGuideModal({ closeModal }) {
       </button>
       <FaMugHot />
       <ol className={styles.modaltxt}>
-        <li>본 기능은 <span className={styles.boldHighlight}>방장만</span> 사용 가능합니다.</li>
         <li>
-          화면 하단의 <span className={styles.boldHighlight}>잡담 주제 버튼</span>을 클릭하면, 모든 참가자의 창에 랜덤한
-          주제가 제시됩니다.
+          본 기능은 <span className={styles.boldHighlight}>방장만</span> 사용
+          가능합니다.
         </li>
         <li>
-          주제를 바꾸고 싶다면, <span className={styles.boldHighlight}>'다른 주제' 버튼</span>을 통해 새로운 주제를 받을 수
-          있습니다.
+          화면 하단의{" "}
+          <span className={styles.boldHighlight}>잡담 주제 버튼</span>을
+          클릭하면, 랜덤한 주제가 제시됩니다.
+        </li>
+        <li>
+          주제를 바꾸고 싶다면,{" "}
+          <span className={styles.boldHighlight}>'다른 주제' 버튼</span>을 통해
+          새로운 주제를 받을 수 있습니다.
+        </li>
+        <li>
+          게임이 끝난 뒤 누르면 카메라 프레임이 원래대로 돌아옵니다.
         </li>
       </ol>
     </>
--- frontend/src/components/Modal/ShoutInSilenceGuideModal.js
+++ frontend/src/components/Modal/ShoutInSilenceGuideModal.js
@@ -20,17 +20,28 @@ function ShoutInSilenceGuideModal({ closeModal }) {
       <FaBell />
       <ol className={styles.modaltxt}>
         <li>
-          방장에게 <span className={styles.boldHighlight}>키워드</span>가 주어집니다.
+          방장에게 <span className={styles.boldHighlight}>키워드</span>가
+          주어집니다.
         </li>
         <li>
-          출제자는 30초 동안 제시어에 대해 <span className={styles.boldHighlight}>음소거 상태</span>로 말을 합니다.
-        </li>
-        <li>출제가가 말을 하는 동안, 나머지 사람들은 출제자의 입모양을 보고 제시어를 <span className={styles.boldHighlight}>추리</span>합니다.</li>
-        <li>
-          정답란에 <span className={styles.boldHighlight}>가장 먼저 답을 제출</span>한 사람과 <span className={styles.boldHighlight}>출제자</span>가 <span className={styles.boldHighlight}>점수</span>를 획득합니다. 출제자도 다른 사람들이 맞추기 쉽도록 열심히 말해 봅시다!
+          방장은 30초 동안 제시어에 대해{" "}
+          <span className={styles.boldHighlight}>음소거 상태</span>로 말을
+          합니다.
         </li>
         <li>
-          제한 시간 동안 답을 맞춘 사람이 없으면, 아무도 점수를 얻지 못하고 다음문제로 넘어갑니다.
+          방장이 말을 하는 동안, 나머지 사람들은 출제자의 입모양을 보고
+          제시어를 <span className={styles.boldHighlight}>추리</span>합니다.
+        </li>
+        <li>
+          정답란에{" "}
+          <span className={styles.boldHighlight}>가장 먼저 답을 제출</span>한
+          사람과 <span className={styles.boldHighlight}>방장</span>이{" "}
+          <span className={styles.boldHighlight}>점수</span>를 획득합니다.
+          출제자도 다른 사람들이 맞추기 쉽도록 열심히 말해 봅시다!
+        </li>
+        <li>
+          제한 시간 동안 답을 맞춘 사람이 없으면, 아무도 점수를 얻지 못하고
+          다음문제로 넘어갑니다.
         </li>
       </ol>
     </>
