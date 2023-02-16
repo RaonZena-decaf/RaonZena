a15cf6386a4866a98f5970802db252c53b5a3c83
choijiyeon <choiwldus@naver.com>
2023-02-16 10:01:35 +0900

    Fix: 팔로우상태보기 수정

--- frontend/src/components/Followings/FollowingListItem.js
+++ frontend/src/components/Followings/FollowingListItem.js
@@ -20,9 +20,9 @@ const FollowingListItem = (props) => {
         <p>{props.userName}</p>
       </div>
       <div>
-        {props.isOnline ==="online" ? (
+        {props.isOnline && (
           <img className={styles.OnlineSize} src="Online.svg" alt="Online" />
-        ) : null}
+        )}
       </div>
     </div>
   );
